bbb3a2f3a031e3ff617738bea9772dceb9a46137
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-16 03:38:40 -0300

    Creating Generate QR Section component

---
+++ src/components/home/sections/generate-qr-section/generate-qr-section.tsx
@@ -0,0 +1,12 @@
+import styles from './generate-qr-section.module.css';
+
+export default function GenerateQRSection() {
+    return (
+        <section className={styles.section}>
+            <h1>Generate QR Code</h1>
+            <div className={styles.container}></div>
+            <aside>
+            </aside>
+        </section>
+    )
+}
