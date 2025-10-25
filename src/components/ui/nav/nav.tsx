6ca8e5c5671ad75c4515e1903b4d617802d1e8ba
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-25 14:33:39 -0300

    Removing account button from Nav
    The account feature will be added in a future version

--- src/components/ui/nav/nav.tsx
+++ src/components/ui/nav/nav.tsx
@@ -1,15 +1,14 @@
-import Link from 'next/link';
+// Styles
 import styles from './nav.module.css';
-import { UserCircle2 } from 'lucide-react';
+
+// Components
+import Link from 'next/link';
 
 export default function Nav() {
     return (
         <nav className={styles.nav}>
             <Link href="/">
                 <h1>Scano QR Generator</h1>
-            </Link>
-            <Link href="/account" className={styles.link}>
-                <UserCircle2 size={34} color='#3454d1' />
             </Link>
         </nav>
     )
