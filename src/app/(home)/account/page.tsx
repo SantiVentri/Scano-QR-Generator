b8e326c5dd8bc65b64bda7b821f658fe93c047de
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-31 02:23:11 -0300

    Creating initial Account page

---
+++ src/app/(home)/account/page.tsx
@@ -0,0 +1,10 @@
+// Styles
+import styles from './account.module.css';
+
+// Components
+
+export default function AccountPage() {
+    return (
+        <main className={styles.page}></main>
+    )
+}
