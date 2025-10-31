256d4605caefe6cb637058e4d534baac0c7bc5a1
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-31 02:23:23 -0300

    Updating Nav component for Account page

--- src/components/ui/nav/nav.tsx
+++ src/components/ui/nav/nav.tsx
@@ -1,14 +1,28 @@
 // Styles
+import { createClient } from '@/utils/supabase/server';
 import styles from './nav.module.css';
 
 // Components
 import Link from 'next/link';
+import React from 'react';
+import { UserCircle2 } from 'lucide-react';
 
-export default function Nav() {
+export default async function Nav() {
+    const supabase = await createClient();
+    const { data: { user }, error } = await supabase.auth.getUser();
+
+    const username = user?.user_metadata?.display_name || null;
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
                 <h1>Scano QR Generator</h1>
+            </Link>
+            <Link href="/account" className={styles.accountLink}>
+                <UserCircle2 size={28} />
+                <p>
+                    {username ? username : 'Account'}
+                </p>
             </Link>
         </nav>
     )
