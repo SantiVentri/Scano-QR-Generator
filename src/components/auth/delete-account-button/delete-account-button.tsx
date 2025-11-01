00ba13af6ab1ee9ae7355292c3c6e9c810357770
Santino Ventrice <santino.ventrice@gmail.com>
2025-11-01 02:41:17 -0300

    Using new Modal for DeleteAccount confirmation

--- src/components/auth/delete-account-button/delete-account-button.tsx
+++ src/components/auth/delete-account-button/delete-account-button.tsx
@@ -8,15 +8,17 @@ import { createClient } from "@/utils/supabase/client";
 import { redirect } from "next/navigation";
 import { useState } from "react";
 import { useToast } from "@/components/ui/ToastProvider";
+import Modal from "@/components/home/modal/modal";
 
 export default function DeleteAccountButton() {
     const supabase = createClient();
     const { showToast } = useToast();
     const [isLoading, setIsLoading] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
 
     const deleteAccount = async () => {
         setIsLoading(true);
-        const { error } = await supabase.rpc('delete_user_account');
+        const { error } = await supabase.rpc('delete_account');
 
         if (error) {
             showToast("Error deleting account. Please try again.", "error");
@@ -24,15 +26,27 @@ export default function DeleteAccountButton() {
             return;
         }
 
+        showToast("Account deleted successfully!", "success");
         redirect("/auth/login")
     }
 
     return (
-        <button
-            className={styles.button}
-            onClick={deleteAccount}
-        >
-            {isLoading ? "Deleting account..." : "Delete Account"}
-        </button>
+        <>
+            <button
+                className={styles.button}
+                onClick={() => setShowAlert(true)}
+            >
+                {isLoading ? "Deleting account..." : "Delete Account"}
+            </button>
+            <Modal
+                isOpen={showAlert}
+                onClose={() => setShowAlert(false)}
+                onConfirm={deleteAccount}
+                title="Confirm delete"
+                message="Are you sure you want to delete your account? This action is irreversible."
+                confirmText="Delete"
+                cancelText="Cancel"
+            />
+        </>
     )
 }
